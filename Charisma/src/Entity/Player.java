483639e34928da52b135b3697710fb3fca03ab1c
jshiraef <joeshiraef@gmail.com>
2014-08-24 02:17:14 -0400

    player class complete; actions now showing on screen. moving, jumping, hovering

--- Charisma/src/Entity/Player.java
+++ Charisma/src/Entity/Player.java
@@ -1,5 +1,13 @@
 package Entity;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import TileMap.TileMap;
+
 public class Player extends MapEntity{
 	
 	// player stuff
@@ -10,7 +18,7 @@ public class Player extends MapEntity{
 	
 	private boolean dead;
 	private boolean flinching;
-	private boolean flinchTime;
+	private long flinchTimer;
 	
 	// fireball
 	private boolean firing; 
@@ -19,16 +27,264 @@ public class Player extends MapEntity{
 	
 //	private ArrayList<CharismaOrb> orbs;
 	
-	// scratch
-	private boolean scratching; 
-	private int scratch;
-	private int scratchRange;
+	// swiping
+	private boolean swiping; 
+	private int swipeDamage;
+	private int swipeRange;
 	
-	// glinding 
-	private boolean gliding;
+	// hovering 
+	private boolean hovering;
+	
+	// animations
+	private ArrayList<BufferedImage[]> sprites;
+	private final int[] numFrames = {2, 8, 1, 2, 4, 2, 5};
+	
+	// animation actions
+	private static final int IDLE = 0;
+	private static final int WALKING = 1;
+	private static final int JUMPING = 2;
+	private static final int FALLING = 3;
+	private static final int HOVERING = 4;
+	private static final int FIRING = 5;
+	private static final int SWIPING = 6;
+	
+	public Player (TileMap tm) {
+		super (tm);
+		width = 30;
+		height = 30;
+		collisionWidth = 20;
+		collisionHeight = 20;
+		
+		moveSpeed = 0.3;
+		maxSpeed = 1.6;
+		stopSpeed = 0.4;
+		fallSpeed = 0.15;
+		maxFallSpeed = 4.0;
+		jumpStart = -4.8;
+		stopJumpSpeed = 0.3;
+		
+		facingRight = true;
+		
+		health = maxHealth = 5;
+		charisma = maxCharisma =  2500;
+		
+		charismaCost = 200;
+		charismaEffectiveness = 5;
+		// orbs = new ArrayList<CharismaOrb>();
+		
+		swipeDamage = 8;
+		swipeRange = 40;
+		
+		// load sprites
+		 try {
+			 
+			 BufferedImage spritesheet = ImageIO.read(getClass().getResourceAsStream("/sprites/Player/playerSprites2.png"));
+			 
+			 sprites = new ArrayList<BufferedImage[]>();
+			 
+			 for (int i = 0; i < 7; i++) {
+				 BufferedImage[] bi = new BufferedImage[numFrames[i]];
+				 for (int j = 0; j < numFrames[i]; j++) {
+					 
+					 if(i != 6) {
+					 bi[j] = spritesheet.getSubimage(j * width, i * height, width, height);
+					 }
+					 else {  
+						 bi[j] = spritesheet.getSubimage(j * width * 2, i * height, width, height);
+					 }
+				 
+				 	}
+				 
+			 sprites.add(bi);
+			 
+			 } 
+		 
+		 }
+		  catch (Exception e) {
+			  e.printStackTrace();
+		  }
+		 
+		 animation = new Animation();
+		 currentAction = IDLE;
+		 animation.setFrames(sprites.get(IDLE));
+		 animation.setDelay(400);
+	}
 	
 	
+	public int getHealth() {
+		return health;
+	}
 	
+	public int getMaxHealth() {
+		return maxHealth;
+	}
 	
+	public int getCharisma () {
+		return charisma;
+	}
+	
+	public int getMaxCharisma() {
+		return maxCharisma;
+	}
+	
+	public void setFiring() {
+		firing = true;
+	}
+	
+	public void setSwiping() {
+		swiping = true;
+	}
+	
+	public void setHovering(boolean b) {
+		hovering = b;
+	}
+	
+	private void getNextPosition() {
+		// movement
+		if(left) {
+			dx -= moveSpeed;
+			if(dx < -maxSpeed) {
+				dx = -maxSpeed;
+			}
+		}
+		else if(right) {
+			dx += moveSpeed;
+			if(dx > maxSpeed) {
+				dx = maxSpeed;
+			}
+		}
+		else {
+			if (dx > 0) {
+				dx -= stopSpeed;
+				if(dx < 0) {
+					dx = 0;
+				}
+			}
+			else if (dx < 0) {
+				dx += stopSpeed;
+				if(dx > 0) {
+					dx = 0;
+				}
+			}
+		}
+		
+		// cannot move while attacking except in air
+		if((currentAction == SWIPING || currentAction == FIRING) && !(jumping || falling)) {
+			dx = 0;
+		}
+		
+		// jumping
+		if(jumping && !falling) {
+			dy = jumpStart;
+			falling = true;
+		}
+		
+		if(falling) {
+			if(dy > 0 && hovering) 
+				dy += fallSpeed * 0.1;
+			else dy += fallSpeed;
+			
+			if(dy > 0)
+				jumping = false;
+			if(dy < 0 && !jumping)
+				dy += stopJumpSpeed;
+			if(dy > maxFallSpeed) 
+				dy = maxFallSpeed;
+		}
+		
+	}
+	
+	public void update() {
+		// update position
+		getNextPosition();
+		checkTileMapCollision();
+		setPosition(xtemp, ytemp);
+		
+		// set animation
+		if(swiping) {
+			if(currentAction != SWIPING) {
+				currentAction = SWIPING;
+				animation.setFrames(sprites.get(SWIPING));
+				animation.setDelay(50);
+				width = 60;
+			}
+		}
+		else if(firing) {
+			if(currentAction != FIRING) {
+				currentAction = FIRING;
+				animation.setFrames(sprites.get(FIRING));
+				animation.setDelay(100);
+				width = 30;
+			}
+		}
+		else if(dy > 0) {
+			if(hovering) {
+				if(currentAction != HOVERING) {
+					currentAction = HOVERING;
+					animation.setFrames(sprites.get(HOVERING));
+					animation.setDelay(100);
+					width = 30;
+				}
+			}
+			else if (currentAction != FALLING) {
+				currentAction = FALLING;
+				animation.setFrames(sprites.get(FALLING));
+				animation.setDelay(100);
+				width = 30;
+			}
+		}
+		else if(dy < 0) {
+			if(currentAction != JUMPING) {
+				currentAction = JUMPING;
+				animation.setFrames(sprites.get(JUMPING));
+				animation.setDelay(-1);
+				width = 30;
+			}
+		}
+		else if (left || right) {
+			if(currentAction != WALKING) {
+				currentAction = WALKING;
+				animation.setFrames(sprites.get(WALKING));
+				animation.setDelay(400);
+				width = 30;
+			}
+		}
+		else {
+			if(currentAction != IDLE) {
+				currentAction = IDLE;
+				animation.setFrames(sprites.get(IDLE));
+				animation.setDelay(400);
+				width = 30;
+			}
+		}
+		animation.update();
+		
+		// set direction
+		if(currentAction != SWIPING && currentAction != FIRING) {
+			if(right) facingRight = true;
+			if(left) facingRight = false;
+		}
+	}
+	
+	public void draw(Graphics2D g) {
+		setMapPosition();
+		 
+		// draw Player
+		
+		if (flinching) {
+			long elapsed = (System.nanoTime() - flinchTimer)/ 1000000;
+			if(elapsed / 100 % 2 == 0) {
+				return;
+				
+			}
+		}
+		
+		if(facingRight) {
+			g.drawImage(animation.getImage(), (int) (x + xmap - width/ 2), (int) (y + ymap - height/2), null);	
+		}
+		else {
+			g.drawImage(animation.getImage(), (int)	(x + xmap - width/2 + width), (int) (y + ymap - height/2), -width, height, null);
+		}
+	}
 
 }
--- Charisma/src/GameState/Village.java
+++ Charisma/src/GameState/Village.java
@@ -2,8 +2,10 @@ package GameState;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 import main.GamePanel;
+import Entity.Player;
 import TileMap.BackGround;
 import TileMap.TileMap;
 
@@ -12,6 +14,8 @@ public class Village extends GameState {
 	
 	private TileMap tileMap;
 	private BackGround bg;
+	
+	private Player player;
 	
 	public Village (GameStateManager gsm) {
 		this.gsm = gsm;
@@ -24,13 +28,19 @@ public class Village extends GameState {
 		tileMap.loadTiles("/tilesets/VillageTiles.png");
 		tileMap.loadMap("/maps/village.map");
 		tileMap.setPosition(0, 0);
+		tileMap.setBetween(1);
 		
 		bg = new BackGround("/Backgrounds/grass.png", 0.1);
+		
+		player = new Player(tileMap);
+		player.setPosition(100, 100);
 		
 	}
 	
 	public void update() {
-		
+		// update player
+		player.update();
+		tileMap.setPosition(GamePanel.WIDTH/ 2 - player.getx(), GamePanel.HEIGHT/2 - player.gety() );
 	}
 	public void draw (Graphics2D g) {
 		
@@ -40,11 +50,56 @@ public class Village extends GameState {
 		// draw tilemap
 		tileMap.draw(g);
 		
+		// draw player
+		player.draw(g);
+		
 	}
 	public void keyPressed(int k) {
 		
+		if(k == KeyEvent.VK_LEFT) {
+			player.setLeft(true);
+		}
+		if(k == KeyEvent.VK_RIGHT) {
+			player.setRight(true);
+		}
+		if(k == KeyEvent.VK_UP) {
+			player.setUp(true);
+		}
+		if(k == KeyEvent.VK_DOWN) {
+			player.setDown(true);
+		}
+		if(k == KeyEvent.VK_W) {
+			player.setJumping(true);
+		}
+		if(k == KeyEvent.VK_E) {
+			player.setHovering(true);
+		}
+		if(k == KeyEvent.VK_R) {
+			player.setSwiping();
+		}
+		if(k == KeyEvent.VK_F) {
+			player.setFiring();
+		}
 	}
 	public void keyReleased(int k) {
+		if(k == KeyEvent.VK_LEFT) {
+			player.setLeft(false);
+		}
+		if(k == KeyEvent.VK_RIGHT) {
+			player.setRight(false);
+		}
+		if(k == KeyEvent.VK_UP) {
+			player.setUp(false);
+		}
+		if(k == KeyEvent.VK_DOWN) {
+			player.setDown(false);
+		}
+		if(k == KeyEvent.VK_W) {
+			player.setJumping(false);
+		}
+		if(k == KeyEvent.VK_E) {
+			player.setHovering(false);
+		}
 		
 	}	
 	
